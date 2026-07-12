bdc3c6a8e37ca786d8aa91dd077aea400afaa739
viknneek <viknneek@gmail.com>
2026-07-12 12:03:52 +0200

    fix: payload

--- src/app/(payload)/admin/[[...segments]]/not-found.tsx
+++ src/app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -1,6 +1,21 @@
 import config from '@payload-config'
 import { NotFoundPage } from '@payloadcms/next/views'
+import { importMap } from '../importMap.js'
 
-export default function NotFound() {
-  return NotFoundPage({ config })
+type Args = {
+  params: Promise<{
+    segments: string[]
+  }>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
+}
+
+export default function NotFound({ params, searchParams }: Args) {
+  return NotFoundPage({
+    config,
+    importMap,
+    params,
+    searchParams,
+  })
 }
--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,14 +1,28 @@
 import config from '@payload-config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import { importMap } from '../importMap.js'
 
 type Args = {
-  params: Promise<{ segments: string[] }>
-  searchParams: Promise<Record<string, string | string[]>>
+  params: Promise<{
+    segments: string[]
+  }>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
 }
 
 export const generateMetadata = ({ params, searchParams }: Args) =>
-  generatePageMetadata({ config, params, searchParams })
+  generatePageMetadata({
+    config,
+    params,
+    searchParams,
+  })
 
 export default function Page({ params, searchParams }: Args) {
-  return RootPage({ config, params, searchParams })
+  return RootPage({
+    config,
+    importMap,
+    params,
+    searchParams,
+  })
 }
