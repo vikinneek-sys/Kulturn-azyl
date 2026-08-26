23082660066b4ff5b5d353e66dd4994115f8a575
viknneek <viknneek@gmail.com>
2026-08-26 17:49:16 +0200

    upravy

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -9,19 +9,19 @@ const categories = [
     description: 'Koncerty, novinky i rozhovory. Poklady skryté v malých klubech, barech nebo teprve hledající své první malé velké pódia.',
   },
   {
-    title: 'Malba / grafika',
-    slug: 'malba-grafika',
-    description: 'Obrazy, grafika, výstavy, skici, plakáty a vizuální bordel s duší.',
+    title: 'Výtvarné umění',
+    slug: 'vytvarne-umeni',
+    description: 'Máš volné místo na zdi? Chceš něco položit na noční stolek? Tady najdeš poctivé umělce ať už hledáš cool obrazy, postery nebo stylovější plastiky než zahradního trpaslíka z Unihobby.',
   },
   {
     title: 'Literatura',
     slug: 'literatura',
-    description: 'Poezie, próza, knihy, autorská čtení a texty, které nekončí u lajku.',
+    description: 'Žádná maturita. Už stačilo Shakespera a Máchy. Přečti si něco od lidí, které znáš a chápeš, ať už chceš romány nebo verše.',
   },
   {
-    title: 'Divadlo / performance',
+    title: 'Divadlo/performance',
     slug: 'divadlo-performance',
-    description: 'Živé umění, divadlo, performance, experiment a tělo v prostoru.',
+    description: 'Člověk nemusí všechno zaznamenat. Něco se dá ukázat na prknech co znamenají svět. Co takhle prkna, která znamenají podsvětí?',
   },
 ]
 
